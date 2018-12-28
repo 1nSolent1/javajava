9c9ac7cd52434345e20f19252ae97c3154f35d58
1nSolent1 <kopo00@mail.ru>
2018-12-28 22:35:37 +0300

    java

--- lesson/src/main/java/main.java
+++ lesson/src/main/java/main.java
@@ -4,11 +4,11 @@ public class main {
 
     private static final String USERNAME = "root";
     private static final String PASSWORD = "1234576";
-    private static final String URL = "jdbc:mysql://localhost:3306/java";
+    private static final String URL = "jdbc:mysql://localhost:3306/java?useSSL=false";
 
     public static void main(String[] args)  {
         try {
-            Class.forName("org.mysql.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
             Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM person");
@@ -17,11 +17,10 @@ public class main {
             resultSet.close();
             statement.close();
             connection.close();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
-
 }
